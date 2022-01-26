3561562a5c524b6d5f4854fd5d8b26e336404280
kennyoungC <96569081+kennyoungC@users.noreply.github.com>
2022-01-26 13:59:09 +0100

    commit
    continuation of introduction to javascript (Creating some functionality to our omnifood project)

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+console.log("hello world");
+
+const myName = "kenneth obi";
+const h1 = document.querySelector(".heading-primary");
+console.log(myName);
+console.log(h1);
+
+// h1.addEventListener("click", function () {
+//   h1.textContent = myName;
+//   h1.style.backgroundColor = "red";
+//   h1.style.padding = "5rem";
+// });
+
+const yearEL = document.querySelector(".year");
+const currentYear = new Date().getFullYear();
+console.log(currentYear);
+yearEL.textContent = currentYear;
 ///////////////////////////////////////////////////////////
 // Fixing flexbox gap property missing in some Safari versions
 function checkFlexGap() {
